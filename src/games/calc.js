fbf45202ba01fa576849fd2d27ede2383da35fbd
Eugene Korshunov <emkorshunov@gmail.com>
2021-09-08 06:19:38 +0500

    refactor progression game: progression func.

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,17 +3,22 @@ import runGame from '../index.js';
 
 const description = 'What is the result of the expression?';
 const operators = ['+', '-', '*'];
-const math = {
-  '+': (a, b) => a + b,
-  '-': (a, b) => a - b,
-  '*': (a, b) => a * b,
+
+const calculate = (num1, num2, operator) => {
+  const math = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+  };
+
+  return math[operator](num1, num2);
 };
 
 const getQuestionAnswer = () => {
   const num1 = getRandom(1, 20);
   const num2 = getRandom(1, 20);
   const operator = operators[getRandom(0, 2)];
-  const result = math[operator](num1, num2);
+  const result = calculate(num1, num2, operator);
   return {
     question: `${num1} ${operator} ${num2}`,
     correctAnswer: String(result),
--- src/games/progression.js
+++ src/games/progression.js
@@ -4,25 +4,31 @@ import runGame from '../index.js';
 const description = 'What number is missing in the progression?';
 const progressionLength = 10;
 
-const getProgression = (startNum, step, hiddenNumPosition) => {
+const getProgression = (startNum, step) => {
   const progression = [];
   const lastNum = startNum + step * (progressionLength - 1);
   for (let i = startNum; i <= lastNum; i += step) {
     progression.push(i);
   }
-  progression[hiddenNumPosition] = '..';
   return progression;
+};
+
+const hideElement = (progression, hiddenNumPosition) => {
+  const progressionWithHidden = [...progression];
+  progressionWithHidden[hiddenNumPosition] = '..';
+  return progressionWithHidden;
 };
 
 const getQuestionAnswer = () => {
   const startNum = getRandom(0, 20);
   const step = getRandom(2, 10);
   const hiddenNumPosition = getRandom(0, progressionLength - 1);
-  const progression = getProgression(startNum, step, hiddenNumPosition);
+  const progression = getProgression(startNum, step);
+  const progressionWithHidden = hideElement(progression, hiddenNumPosition);
   const answer = startNum + hiddenNumPosition * step;
 
   return {
-    question: progression.join(' '),
+    question: progressionWithHidden.join(' '),
     correctAnswer: String(answer),
   };
 };
